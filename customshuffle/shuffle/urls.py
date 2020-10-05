52e5b398ee97c953f31bb66d0fc42b0f0e975c95
cjokeefe <chrisokeefe4@gmail.com>
2020-10-05 13:16:01 -0400

    Displays all songs, numbered rows, playlist art, displays only owned public playlists, paginated list of playlists

--- customshuffle/shuffle/urls.py
+++ customshuffle/shuffle/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 	path('', views.index, name="index"),
 	path('callback/', views.callback, name="callback"),
-	path('playlists/', views.ListPlaylists, name="playlists"),
+	path('playlists/<int:offset>/', views.ListPlaylists, name="playlists"),
 	path('playlists/<str:pl_id>/submit/', views.ShufflePlaylist, name="submit"),
 	path('playlists/<str:pl_id>/', views.PlaylistView, name="view-playlist"),
 ]
--- customshuffle/shuffle/views.py
+++ customshuffle/shuffle/views.py
@@ -16,7 +16,9 @@ def index(request):
 
 	username=str(request.session['id'])
 
-	auth_manager = spotipy.oauth2.SpotifyOAuth(scope='playlist-modify-public user-top-read playlist-read-private', username=username, show_dialog=True)
+	scope = 'playlist-modify-public user-top-read playlist-modify-private'
+
+	auth_manager = spotipy.oauth2.SpotifyOAuth(scope=scope, username=username, show_dialog=True)
 
 	if request.GET.get('code'):
 		code = request.GET.get('code')
@@ -51,21 +53,26 @@ def index(request):
 	return render(request, 'shuffle/home.html', context)
 
 
-def ListPlaylists(request):
+def ListPlaylists(request, offset):
 	username = str(request.session['id'])
 	auth_manager = spotipy.oauth2.SpotifyOAuth(username=username)
 	if not auth_manager.get_cached_token():
 		print("Did not get cached token")
 		return HttpResponseRedirect('index')
 	spotify = spotipy.Spotify(auth_manager=auth_manager)
-	playlists = spotify.current_user_playlists(limit=20)['items']
+	playlists = spotify.current_user_playlists(limit=20, offset=offset)['items']
+
 	plList = []
 	for pl in playlists:
-		item = {'name': pl['name'], 'id': pl['id']}
-		plList.append(item)
+		if(pl['owner']['id'] == spotify.me()['id']):
+			item = {'name': pl['name'], 'id': pl['id']}
+			plList.append(item)
 
 	context = {
 		'playlists': plList,
+		'offset': offset,
+		'next': offset+20,
+		'prev': offset-20,
 	}
 
 	#print(json.dumps(playlists, sort_keys=True, indent=4))
@@ -102,22 +109,48 @@ def PlaylistView(request, pl_id):
 		return HttpResponseRedirect('index')
 	spotify = spotipy.Spotify(auth_manager=auth_manager)
 
-	pl = spotify.playlist(pl_id, fields='name,tracks(items(track(name,artists.name,album.name)))')
+
+	pl = spotify.playlist(pl_id, fields='name')
+	try:
+		pl_img = spotify.playlist_cover_image(pl_id)[1]['url']
+	except:
+		pl_img = None
 	#print(json.dumps(pl, sort_keys=True, indent=4))
 
+	done = False
+	result = []
+	offset = 0
+	while(not done):
+		# Get playlist tracks
+		pl_track_items = spotify.playlist_tracks(playlist_id=pl_id,
+							fields='items(track(name,artists.name,album.name))', offset=offset)['items']
+
+		result.extend(pl_track_items)
+
+		if(len(pl_track_items) < 100):
+			done = True
+
+		offset = offset + 100
+
+	#print(json.dumps(result, sort_keys=True, indent=4))
+
 	tracks = []
-	for item in pl['tracks']['items']:
+	counter = 1
+	for item in result:
 		trackItem = {
+			'trackNum': counter,
 			'name': item['track']['name'],
 			'artist': item['track']['artists'][0]['name'],
-			'album': item['track']['album']['name']
+			'album': item['track']['album']['name'],
 		}
 		tracks.append(trackItem)
+		counter += 1
 
 	context = {
 		'pl_name': pl['name'],
 		'tracks': tracks,
 		'pl_id': pl_id,
+		'pl_img': pl_img,
 	}
 	return render(request, 'shuffle/view_playlist.html', context)
 
